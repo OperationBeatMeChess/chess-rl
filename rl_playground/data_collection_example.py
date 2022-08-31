153381d9e3839174807cd05cbc29a63e7f68f5c3
Dawson Horvath <horvath.dawson@gmail.com>
2022-08-31 15:14:45 -0700

    added in loading of state observation. Also changed observation

--- rl_playground/data_collection_example.py
+++ rl_playground/data_collection_example.py
@@ -1,22 +1,39 @@
 import gym
 import obm_gym
+import time
 
 env = gym.make("Chess-v0")
 env.reset()
 
-terminal = False
-observations = []
+# terminal = False
+# observations = []
 
-while not terminal:
-    action = env.action_space.sample()
-    observation, reward, terminal, info = env.step(action)
-    print(observation)
-    player = info['player']
-    observations.extend({'observation': observation, 'player': player})
-    env.render()
+# while not terminal:
+#     action = env.action_space.sample()
+#     observation, reward, terminal, info = env.step(action)
+#     print(observation)
+#     player = info['player']
+#     observations.extend({'observation': observation, 'player': player})
+#     env.render()
 
-# action = env.action_space.sample()
-# observation, reward, terminal, info = env.step(action)
-# print(action)
+action = env.action_space.sample()
+observation, reward, terminal, info = env.step(action)
+print(observation)
+env.render()
+time.sleep(5)
+action = env.action_space.sample()
+_ = env.step(action)
+action = env.action_space.sample()
+_ = env.step(action)
+action = env.action_space.sample()
+_ = env.step(action)
+
+env.set_board_state(observation)
+env.render()
+time.sleep(5)
+action = env.action_space.sample()
+_ = env.step(action)
+
+
   
 env.close()
