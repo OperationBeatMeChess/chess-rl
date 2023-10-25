2a465dcce96d6307a575edc4d648aea3fb5e7fc2
KeithG33 <keith.gordon9@gmail.com>
2023-10-24 17:09:52 -0700

    does weighted average q value merging

--- monte-carlo-tree-search-NN/merge_trees.py
+++ monte-carlo-tree-search-NN/merge_trees.py
@@ -1,18 +1,22 @@
 def merge_trees(tree1, tree2):
     """ Merges tree2 into tree1. """
     
-    # Merge state-action qualities and visits
-    for key, value in tree2._quality_states_actions.items():
-        if key in tree1._quality_states_actions:
-            tree1._quality_states_actions[key] += value
-        else:
-            tree1._quality_states_actions[key] = value
 
     for key, value in tree2._number_of_visits_states_actions.items():
         if key in tree1._number_of_visits_states_actions:
             tree1._number_of_visits_states_actions[key] += value
         else:
             tree1._number_of_visits_states_actions[key] = value
+
+    # Merge state-action q-values. Does a weighted average based on number of visits
+    for key, value in tree2._quality_states_actions.items():
+        if key in tree1._quality_states_actions:
+            total_visits = tree1._number_of_visits_states_actions[key]
+            tree1_contrib = tree1._quality_states_actions[key] * (total_visits - tree2._number_of_visits_states_actions[key])
+            tree2_contrib = value * tree2._number_of_visits_states_actions[key]
+            tree1._quality_states_actions[key] = (tree1_contrib + tree2_contrib) / total_visits
+        else:
+            tree1._quality_states_actions[key] = value
 
     # Merge state visits
     for key, value in tree2._number_of_visits_states.items():
