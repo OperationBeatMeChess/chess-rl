df694c0ec1764c77e487ad862bd76e24d8f22afb
KeithG33 <keith.gordon9@gmail.com>
2023-11-19 18:28:28 -0800

    cleaned search.py

--- monte-carlo-tree-search-NN/search.py
+++ monte-carlo-tree-search-NN/search.py
@@ -1,8 +1,4 @@
 import numpy as np
-from collections import defaultdict
-from abc import ABC, abstractmethod
-import gym
-from collections import deque
 
 import torch
 
@@ -39,19 +35,13 @@ class MonteCarloTreeSearch:
     def search(self, init_state, init_obs, simulations_number=10_000):
         for itr in range(simulations_number):
             self.game_state.set_string_representation(init_state)
-            # length = 20
-            # traversal_queue = deque(np.array([np.zeros(64) for i in range(length)]), maxlen=length)
-            traversal_queue = None
-            self.search_iteration(self.game_state, game_obs=init_obs, traversal_queue=traversal_queue)
+            self.search_iteration(self.game_state, game_obs=init_obs)
 
         self.game_state.set_string_representation(init_state)
         return self.best_action(init_state, c_param=0.)
 
-    def search_iteration(self, game_state, game_obs, traversal_queue=None, c_param=1.4):
+    def search_iteration(self, game_state, game_obs, c_param=1.4):
         state = game_state.get_string_representation()
-
-        if traversal_queue is not None:
-            traversal_queue.append(game_obs[0].flatten())
 
         if state not in self._is_terminal_states:
             self._is_terminal_states[state] = game_state.game_result()
@@ -69,19 +59,12 @@ class MonteCarloTreeSearch:
             self._current_player[state] = game_state.current_player
             self._previous_player[state] = game_state.previous_player
 
-            # player_at_leaf = game_obs[1] 
             player_at_leaf = self._current_player[state]
 
             # 1 current state wins, -1 previous state wins
-            # predicted_outcome = np.random.rand()
-            # _, predicted_outcome = model(game_obs[0])
             with torch.no_grad():
-                _, predicted_outcome = self.nnet(game_obs[0])
-            # predicted_outcome = self.nnet.predict(np.array(traversal_queue, dtype=np.uint8).reshape((20, 1, 64)))
-            # predicted_outcome = self.nnet.predict(np.array(traversal_queue, dtype=np.uint8)[np.newaxis, ...])[-1]
+                predicted_outcome = self.nnet(game_obs[0])[1].item()
 
-            # print('current player:', player_at_leaf, 'player at start:', game_state.starting_player, 'predicted outcome:', predicted_outcome)
-            # predicted_outcome = predicted_outcome  * 2 - 1
             return player_at_leaf, predicted_outcome
 
         best_action, best_ucb = self.best_action(state, c_param=c_param)
@@ -91,8 +74,7 @@ class MonteCarloTreeSearch:
         observation, reward, terminated, truncated, info = game_state.step(best_action)
         player_at_leaf, predicted_outcome = self.search_iteration(
             game_state=game_state,
-            game_obs=observation,
-            traversal_queue=traversal_queue)
+            game_obs=observation)
 
         # result is -1 if previous player won, and 1 if current player won.
         result = predicted_outcome if player_at_leaf == self._current_player[state] else - \
@@ -113,6 +95,7 @@ class MonteCarloTreeSearch:
     def best_action(self, state, c_param=1.4):
         best_ucb = -np.inf
         best_action = None
+        
         # find highest ucb action
         N = self._number_of_visits_states[state]
         LOGN = np.log(N)
